e4136a7011d4bb6c6f54fb2eb22c801893ccdc33
Daniyal Gabitov <danuy@list.ru>
2017-02-18 15:28:16 +0300

    Add list and form

--- src/app.js
+++ src/app.js
@@ -1,1 +1,85 @@
-console.log('test');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {ItemStorage} from './services/item-storage';
+
+let storage = new ItemStorage(localStorage);
+
+class EnterDataComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClearAll = this.handleClearAll.bind(this);
+    this.state = {
+      items: storage.getItems('items', true)
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let formData = {};
+    Array.prototype.slice.call(document.querySelectorAll('input'))
+      .forEach(el => formData[el.name] = el.value);
+    storage.addItem('items', formData);
+    e.target.reset();
+    this.setState({items: storage.getItems('items', true)})
+  }
+
+  handleClearAll() {
+    storage.deleteItems(
+      this.state.items.map((item) => {
+        return item.id;
+      })
+    );
+    this.setState({items: storage.getItems('items', true)})
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="item"> Item </label>
+          <input name="item" type="string" id="item"/>
+          <label htmlFor="sum"> Sum </label>
+          <input name="sum" type="number" id= "sum"/>
+          <button>Send</button>
+          <button type="button" onClick={this.handleClearAll}>Clear All</button>
+        </form>
+        <ListComponent items={this.state.items}/>
+      </div>
+    );
+  }
+};
+
+class ListItemComponent extends React.Component {
+  render() {
+    const {item, sum} = this.props.item;
+    return (
+      <div>
+        <div>item: {item}</div>
+        <div>sum: {sum}</div>
+      </div>
+    );
+  }
+};
+
+class ListComponent extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map((item) => {
+          return (
+            <li key={item.id}>
+              <ListItemComponent item={item}/>
+            </li>
+          )
+        })}
+      </ul>
+    );
+  }
+};
+
+ReactDOM.render(
+  <EnterDataComponent/>,
+  document.getElementById('app')
+);
---
+++ src/services/item-storage.js
@@ -0,0 +1,77 @@
+export class ItemStorage {
+  static setStaticProperties() {
+    ItemStorage.instanceCounter = ItemStorage.instanceCounter || 0;
+  }
+
+  constructor(storage) {
+    ItemStorage.setStaticProperties();
+
+    if (ItemStorage.instanceCounter === 1) {
+      throw new Error('ItemStorage already has instance');
+    } else {
+      if (storage) {
+        ++ItemStorage.instanceCounter;
+        this.storage = storage;
+      } else {
+        throw new Error('ItemStorage constructor expected a storage parameter');
+      }
+    }
+
+    if (!this.getIDCounter()) {
+      this.storage.setItem('IDCounter', 0);
+    }
+  }
+
+  getItems(table, isArray, state) {
+    let items = JSON.parse(this.storage.getItem(table) || '{}');
+    let collection;
+    if (isArray) {
+      collection = [];
+      for (let key in items) {
+        let item = items[key];
+        if (item.state === state) {
+          item.id = key;
+          collection.push(item);
+        }
+      }
+    } else {
+      collection = {};
+      for (let key in items) {
+        if (items[key].state === state) { collection[key] = items[key]; }
+      }
+    }
+    return collection;
+  }
+
+  deleteItems(itemsArray) {
+    let items = this.getItems();
+    itemsArray.forEach((id) => {
+      delete items[id];
+    });
+    this.storage.setItem('items', JSON.stringify(items));
+  }
+
+  addItem(table, itemObject) {
+    let IDCounter = this.incrementIDCounter();
+    let items = this.getItems(table);
+    items[IDCounter] = itemObject;
+    this.storage.setItem(table, JSON.stringify(items));
+    return {[IDCounter]: itemObject};
+  }
+
+  updateItemState(id, state) {
+    let items = this.getItems();
+    items[id].state = state;
+    this.storage.setItem('items', JSON.stringify(items));
+  }
+
+  getIDCounter() {
+    return this.storage.getItem('IDCounter');
+  }
+
+  incrementIDCounter() {
+    let IDCounter = this.getIDCounter();
+    this.storage.setItem('IDCounter', ++IDCounter);
+    return IDCounter;
+  }
+}
